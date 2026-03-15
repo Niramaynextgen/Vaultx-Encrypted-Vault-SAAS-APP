8ed1a06f218c20943033a19c93e451ac31dafaef
Niramaynextgen <niramayshri0501@gmail.com>
2026-03-15 20:43:09 +0530

    Add python-dotenv to requirements

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','fallback-dev-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','localhost').split(',')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
